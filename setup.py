11901c74899f1e9e6d4a645f0fd607736540ea84
zuber <marek@stepniowski.com>
2009-10-15 19:22:28 +0200

    Bumped version to 0.2.dev and added tagging of releases with date.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
  
 setup(
     name='django-cas-provider',
-    version='0.1dev',
+    version='0.2',
     description='A "provider" for the Central Authentication Service (http://jasig.org/cas)',
     author='Chris Williams',
     author_email='chris@nitron.org',
