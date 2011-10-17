4a479a2031c6d6f96c876e09f6792f96a36da134
Sebastian Annies <sebastian.annies@googlemail.com>
2011-10-17 19:10:58 +0200

    add admins for proxy related tickets

--- cas_provider/admin.py
+++ cas_provider/admin.py
@@ -6,9 +6,24 @@ class ServiceTicketAdmin(admin.ModelAdmin):
     list_display = ('user', 'service', 'created')
     list_filter = ('created',)
 
+class ProxyTicketAdmin(admin.ModelAdmin):
+    list_display = ('user', 'service', 'created')
+    list_filter = ('created',)
+
 class LoginTicketAdmin(admin.ModelAdmin):
     list_display = ('ticket', 'created')
     list_filter = ('created',)
 
+class ProxyGrantingTicketAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'created')
+    list_filter = ('created',)
+
+class ProxyGrantingTicketIOUAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'created')
+    list_filter = ('created',)
+
 admin.site.register(ServiceTicket, ServiceTicketAdmin)
+admin.site.register(ProxyTicket, ProxyTicketAdmin)
 admin.site.register(LoginTicket, LoginTicketAdmin)
+admin.site.register(ProxyGrantingTicket, ProxyGrantingTicketAdmin)
+admin.site.register(ProxyGrantingTicketIOU, ProxyGrantingTicketIOUAdmin)
